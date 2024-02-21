55946e4c601c7884b7781b5a46b7d90d8226566f
deepanshkhurana <deepanshkhurana@outlook.com>
2024-02-21 12:59:12 +0530

    chore: update dependencies for project

--- dependencies.R
+++ dependencies.R
@@ -1,3 +1,8 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(rhino)
 library(usethis)
+library(httr2)
+library(magrittr)
+library(reactable)
+library(rsconnect)
+library(shinycssloaders)
